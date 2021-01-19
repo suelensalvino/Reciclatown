ebeb0f6d36a1255cd38bb0408f62d5a2898a3488
Suelen Salvino <suelensalvinocs@gmail.com>
2021-01-18 23:58:16 -0300

    GH-3 Model e seeder tabela produto_descarte

---
+++ app/Http/Controllers/ProdutoDescarteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\produto_descarte;
+use Illuminate\Http\Request;
+
+class ProdutoDescarteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\produto_descarte  $produto_descarte
+     * @return \Illuminate\Http\Response
+     */
+    public function show(produto_descarte $produto_descarte)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\produto_descarte  $produto_descarte
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(produto_descarte $produto_descarte)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\produto_descarte  $produto_descarte
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, produto_descarte $produto_descarte)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\produto_descarte  $produto_descarte
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(produto_descarte $produto_descarte)
+    {
+        //
+    }
+}
---
+++ database/factories/ProdutoDescarteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\produto_descarte;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProdutoDescarteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = produto_descarte::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
