fb127a42a4fd28f44a5023594dcb380e4288a3a6
danilom2 <danilo280102@gmail.com>
2021-01-25 20:47:43 -0300

    GH-15 Corrigindo erro ao execultar comando reverse

--- app/Models/Coletor.php
+++ app/Models/Coletor.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Coletor extends Model
 {
     use HasFactory;
+
+    public function users(){
+
+    	return $this->belongsTo(User::class);
+
+    }
+
+    public function agendamentoDescartes(){
+
+    	return $this->hasMany(AgendamentoDescarte::class);
+
+    }
 }
