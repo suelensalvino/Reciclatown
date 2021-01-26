a22bf0a2ef2c9004a49617cb74a3cd491fadfd99
danilom2 <danilo280102@gmail.com>
2021-01-25 21:27:07 -0300

    GH-24 Chamando as Seeders no database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        $this->call([
+        	ProdutoDescarteSeeder::class,
+            ColetorSeeder::class,
+            DescarteUsuarioSeeder::class,
+            AgendamentoDescarteSeeder::class,
+
+        ]);
     }
 }
