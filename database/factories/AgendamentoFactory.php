e38fbd593ffb90a5ccdba063f3df254943e80b40
danilom2 <danilo280102@gmail.com>
2021-01-29 23:25:08 -0300

    GH-33 Melhorando nomenclatura do Factory AgendamentoDes...

--- database/factories/AgendamentoFactory.php
+++ database/factories/AgendamentoFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\AgendamentoDescarte;
+use App\Models\Agendamento;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class AgendamentoDescarteFactory extends Factory
+class AgendamentoFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = AgendamentoDescarte::class;
+    protected $model = Agendamento::class;
 
     /**
      * Define the model's default state.
