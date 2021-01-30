7640a26188ec7f21778069df923e4a06a6734a23
danilom2 <danilo280102@gmail.com>
2021-01-30 00:26:15 -0300

    GH-39 Melhorando nomenclatura da Seed ProdutoDescarteSee... e melhorando o código utilizando Collection

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Categoria;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (User::all() as $user){
+        	$categorias = collect(['Computador','Celular','Memória Ram','Placa Mãe','Notebook', 'Monitor', 'Periféricos'])->random(5);
+        
+        foreach ($categorias as $categoria){
+            Categoria::create([
+                'tipo' => $categoria,
+                'user_id' => $user->id,
+                ]);
+            } 
+        }
+    }
+}
