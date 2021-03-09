4e78d32855e88ca5e3b8a4cbd023fe1f4c2071cb
everthonh <everthon.henrique.75@gmail.com>
2021-03-08 23:00:50 -0300

    GH-62 Listando dados do usuario

---
+++ resources/views/dados.blade.php
@@ -0,0 +1,87 @@
+	<div class=" py-2 px-4 grid grid-cols-5  rounded  p-3 m-0.5  cursor-pointe" >
+
+		<div class=" m-3   text-left">
+			<h6>Nome: de usuário:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong><p> {{ Auth::user()->name}} </p></strong>
+			</div>
+		</div>
+		<div class=" m-3   text-left ">
+			<h6>E-mail:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong><p> {{ Auth::user()->email}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Nascimento:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->nascimento}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Tipo:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->tipo}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>UF:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->uf}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Cidade:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->cidade}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Bairro:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->bairro}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Cep:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->cep}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Logradouro:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->logradouro}} </p></strong>
+			</div>
+		</div>
+
+		<div class=" m-3   text-left ">
+			<h6>Complemento:</h6>
+			<div class="text-black text-opacity-100 ">
+				<strong> <p> {{ Auth::user()->complemento}} </p></strong>
+			</div>
+		</div>
+		@if(Auth::user()->tipo == "Coletor(a)")
+
+			<div class=" m-3   text-left ">
+			<h6>Perfil:</h6>
+				<div class="text-black text-opacity-100 ">
+					<strong> <p> {{ Auth::user()->coletors->perfil}} </p></strong>
+				</div>
+			</div>
+
+			<div class=" m-3   text-left ">
+				<h6>Telefone:</h6>
+				<div class="text-black text-opacity-100 ">
+					<strong> <p> {{ Auth::user()->coletors->telefone}} </p></strong>
+				</div>
+			</div>
+		@endif
+	</div>
--- resources/views/produto.blade.php
+++ resources/views/produto.blade.php
@@ -3,6 +3,7 @@
   <div class="mb-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
     <div class="p-6 bg-white border-b border-gray-200">
      {{ __('Informações do usuário') }}
+     @include('dados')
    </div>
  </div>
  <!--Informações produto-->
