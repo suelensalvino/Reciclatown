491031a862fd923f10c2602faa8a63f137b7c30c
desireefernandes <desireefernandes00@gmail.com>
2021-03-12 13:45:21 -0300

    GH-27 Atualizando interface Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
               <span class="block text-blue-900  xl:inline"> é um serviço on-line de descarte e coleta de lixo eletrônico</span>
             </h1>
             <p class="mt-3 text-base text-blue-900  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-              Nosso intuito é fazer da tecnologia nossa aliada a sustentabilidade.
+              Nosso intuito é fazer da tecnologia nossa aliada na sustentabilidade.
             </p>
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center ">
               <div class="rounded-md shadow">
@@ -76,7 +76,7 @@
                   Nosso objetivo
                 </dt>
                 <dd class="mt-2 text-base text-white">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                  Queremos fornecer a praticidade para coletar produtos eletrônicos e a facilidade em descartá-los. Assim, esse projeto surgiu com o pensamento de criar um sistema focado 100% em reciclagem. Desse modo, o Reciclatown visa ser um sistema que atenda ambos os usuários, sendo eles coletores ou distribuidores, de maneira rápida e segura.
                 </dd>
               </div>
             </div>
@@ -87,7 +87,7 @@
                   Como funciona
                 </dt>
                 <dd class="mt-2 text-base text-white">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. 
+                  Nosso sistema disponibiliza a inscrição como coletor ou usuário comum. Posto isso, após adentrar ao site é possivel como usuário comum cadastrar produtos para descarte ou como coletor vizualizar os produtos disponíveis e agendar a retirada com o usuário que está fornecendo o produto. Viu? De modo simples e prático o Reciclatown conecta coletores e doadores fazendo da tecnologia aliada na sustentabilidade.
                 </dd>
               </div>
             </div>
@@ -96,7 +96,7 @@
               <div class="ml-4">
                 <dt class="text-lg leading-6 font-bold text-white">Porque descartar um eletrônico?</dt>
                 <dd class="mt-2 text-base text-white">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. 
+                  Materiais como placas de circuitos, entre outros, possuem quantidades significativas de mercúrio, chumbo e cádmio, metais estes que são altamente poluentes e que afetam tanto na qualidade do solo como da água. Portanto, o Reciclatown por meio de poucos cliques te ajuda a descartar adequadamente ou coletar qualquer eletrônico para reutilização ou qualquer outro objetivo. 
                 </dd>
               </div>
             </div>
@@ -106,7 +106,7 @@
     </div>
 
     <!-- Section statistic -->
-    <div class="py-14 bg-red-400">
+      <!-- <div class="py-14 bg-red-400">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="lg:text-center">
           <p class="mt-2 text-3xl leading-8 tracking-tight text-white sm:text-4xl">
@@ -119,8 +119,8 @@
             <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-4 md:gap-x-8 md:gap-y-10">
               <div class="flex-shrink-0">
                 <div class="ml-4">
-                  <dt class="text-lg sm:text-center leading-6 font-medium text-white">Lorem ipsum</dt>
-                  <dd class="mt-2 text-3x1 text-blue-900 sm:text-4xl sm:text-center">80%</dd>
+                  <dt class="text-lg sm:text-center leading-6 font-medium text-white">mais produziu lixo eletrônico em 2019</dt>
+                  <dd class="mt-2 text-3x1 text-blue-900 sm:text-4xl sm:text-center">5º País</dd>
                 </div>
               </div>
 
@@ -140,15 +140,15 @@
 
               <div class="flex-shrink-0">
                 <div class="ml-4">
-                  <dt class="text-lg sm:text-center leading-6 font-medium text-white">Lorem ipsum</dt>
-                  <dd class="mt-2 text-3x1 text-white sm:text-4xl sm:text-center">80%</dd>
+                  <dt class="text-lg sm:text-center leading-6 font-medium text-white">Lixo eletrônico coletado e reciclado em 2019</dt>
+                  <dd class="mt-2 text-3x1 text-white sm:text-4xl sm:text-center">17,4%</dd>
                 </div>
               </div> 
             </dl>
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <!-- Section red -->
       <!--   <div class="bg-blue-900">
@@ -270,7 +270,7 @@
             </p>
             <!--Cleicy-->
             <div class="p-4 relative">
-              <div class="mb-32 flex items-center flex-col md:flex-row justify evenly">
+              <div class="mb-32 flex items-center flex-col md:flex-row justify-center evenly">
                 <div class="p-4 relative">
                   <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
 
@@ -412,7 +412,10 @@
               </div>
             </div>
           </div>
-          <!--Eduardo-->
+            </div>
+            <!--outra linha-->
+             <div class="flex items-center flex-col md:flex-row justify-center evenly">
+              <!--Eduardo-->
           <div class="p-4 relative">
             <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
               <a href="#" class="block relative">
@@ -459,9 +462,6 @@
              </div>
             </div>
           </div>
-            </div>
-            <!--outra linha-->
-             <div class="flex items-center flex-col md:flex-row justify evenly">
                <!--Everthon-->
                   <div class="p-4 relative">
                     <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
@@ -509,100 +509,6 @@
                     </div>
                   </div>
                 </div>
-                <!--Fábio-->
-                <div class="p-4 relative">
-                  <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
-                    <a href="#" class="block relative">
-                      <img alt="profil" src="../img/fabio.jpg" class="mx-auto object-cover rounded-lg h-40 w-40  border-2 border-white dark:border-gray-800"/>
-                    </a>
-                  </div>
-                  <div class="bg-white dark:bg-gray-800 rounded-lg shadow px-8 py-4 pt-24">
-                    <div class="text-center">
-                      <p class="text-2xl text-gray-800 dark:text-white">
-                        Fábio Moura
-                      </p>
-                      <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
-                        Back-end
-                      </p>
-                    </div>
-                    <div class="pt-8 flex border-t border-gray-200 w-40 mx-auto text-gray-500 items-center justify-center">
-                      <!--Linkedin-->
-                      <a href="https://www.linkedin.com/in/f%C3%A1bio-moura-813b3a149/">
-                        <svg width="30" height="30" fill="currentColor" class="mr-2 text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M477 625v991h-330v-991h330zm21-306q1 73-50.5 122t-135.5 49h-2q-82 0-132-49t-50-122q0-74 51.5-122.5t134.5-48.5 133 48.5 51 122.5zm1166 729v568h-329v-530q0-105-40.5-164.5t-126.5-59.5q-63 0-105.5 34.5t-63.5 85.5q-11 30-11 81v553h-329q2-399 2-647t-1-296l-1-48h329v144h-2q20-32 41-56t56.5-52 87-43.5 114.5-15.5q171 0 275 113.5t104 332.5z">
-                          </path>
-                        </svg>
-                      </a>
-                      <!--Git-->
-                      <a href="https://github.com/FabioMouradeFraga">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="mr-2 text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200" viewBox="0 0 1792 1792">
-                          <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z">
-                          </path>
-                        </svg>
-                      </a>
-                      <!--Gmail-->
-                      <a href="#">
-                        <svg width="30" height="30" fill="currentColor" class="mr-2 text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                        viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                        <g>
-                          <g>
-                            <path d="M464,64h-16H64H48C21.504,64,0,85.504,0,112v16v256v16c0,26.496,21.504,48,48,48h16h384h16c26.496,0,48-21.504,48-48v-16
-                            V128v-16C512,85.504,490.496,64,464,64z M407.488,96L256,215.616L104.512,96H407.488z M448,416H64V148.672l192,147.68L448,148.64
-                            V416z"/>
-                          </g>
-                        </g>
-                      </svg>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <!--Huanderson-->
-              <div class="p-4 relative">
-                <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
-                  <a href="#" class="block relative">
-                    <img alt="profil" src="../img/huanderson.jpg" class="mx-auto object-cover rounded-lg h-40 w-40  border-2 border-white dark:border-gray-800"/>
-                  </a>
-                </div>
-                <div class="bg-white dark:bg-gray-800 rounded-lg shadow px-8 py-4 pt-24">
-                  <div class="text-center">
-                    <p class="text-2xl text-gray-800 dark:text-white">
-                      Huanderson Lima
-                    </p>
-                    <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
-                      Back-end
-                    </p>
-                  </div>
-                  <div class="pt-8 flex border-t border-gray-200 w-40 mx-auto text-gray-500 items-center justify-center">
-                    <!--Linkedin-->
-                    <a href="#">
-                      <svg width="30" height="30" fill="currentColor" class="mr-2 text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M477 625v991h-330v-991h330zm21-306q1 73-50.5 122t-135.5 49h-2q-82 0-132-49t-50-122q0-74 51.5-122.5t134.5-48.5 133 48.5 51 122.5zm1166 729v568h-329v-530q0-105-40.5-164.5t-126.5-59.5q-63 0-105.5 34.5t-63.5 85.5q-11 30-11 81v553h-329q2-399 2-647t-1-296l-1-48h329v144h-2q20-32 41-56t56.5-52 87-43.5 114.5-15.5q171 0 275 113.5t104 332.5z">
-                        </path>
-                      </svg>
-                    </a>
-                    <!--Git-->
-                    <a href="https://github.com/huandersonlima">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="mr-2 text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200" viewBox="0 0 1792 1792">
-                        <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z">
-                        </path>
-                      </svg>
-                    </a>
-                    <!--Gmail-->
-                    <a href="#">
-                      <svg width="30" height="30" fill="currentColor" class="mr-2 text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                      viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                      <g>
-                        <g>
-                          <path d="M464,64h-16H64H48C21.504,64,0,85.504,0,112v16v256v16c0,26.496,21.504,48,48,48h16h384h16c26.496,0,48-21.504,48-48v-16
-                          V128v-16C512,85.504,490.496,64,464,64z M407.488,96L256,215.616L104.512,96H407.488z M448,416H64V148.672l192,147.68L448,148.64
-                          V416z"/>
-                        </g>
-                      </g>
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </div>
             <!--Suelen-->
             <div class="p-4 relative">
               <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
